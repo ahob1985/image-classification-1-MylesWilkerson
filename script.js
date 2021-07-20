1153d3ba27700767b1063abb992701b298394ca9
MylesWilkerson <67336585+MylesWilkerson@users.noreply.github.com>
2021-07-20 20:55:41 +0000

    did the steps and changed step 5

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-// Author:
+// Author: Myles Wilkerson
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
 
 // Global ML Variables
-//let mobilenet;
+let mobilenet;
+let img;
 
 function setup() {
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 400);
+  canvas.parent(canvasDiv);
 
+  textDiv = createDiv();
+  textP = createP("Model loading, please wait....");
+
+  textP.parent(textDiv);
+
+  img = loadImage("images/man.jpg", imageLoaded);
 }
 
 function draw() {
 
 }
 
-function imageLoaded() {
-
+function imageLoaded() { 
+  // draw image to canvas
+  image(img, 0, 0, width, height);
+  // initialize the model 
+  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
 }
 
 function modelReady() {
-
+  mobilenet.classify(canvas, gotResults);
 }
 
 function gotResults(error, results) {
+  if(error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    let label = results[0].label;
+    let confidence = round(results[0].confidence, 2);
+    textP.html("Label: " + label + " - Confidence " + confidence);
 
+
+
+  }
 }
